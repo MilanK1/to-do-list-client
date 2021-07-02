ea1a1e96a9dbf65af30200f7954f534f13388e5e
MilanK1 <54515638+MilanK1@users.noreply.github.com>
2021-07-01 18:06:49 -0700

    The Client side works not as indended, will be fixed!

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,9 +1,19 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Axios from 'axios'
-export default function List(el){
+export default function List(props){
     const [addList, setAddList]=useState([])
+    useEffect(()=>{
+        Axios.get(`http://localhost:8001/getAllToDo`).then(res=>{
+            setAddList([...res.data])
+            console.log(res)
+        })
+    },[])
+
+
+
     const [inpValue, setInpValue]=useState("")
     const addListButton = ()=>{
+
  setAddList([...addList, {text: inpValue, id:"", pos:0}])
  setInpValue("")
 // Functions go here
@@ -11,22 +21,17 @@ export default function List(el){
 
     }
 
-    const ToDo = ()=>{
-        Axios.post(`http://localhost:8001/AddToDo`, {
-            inputValue: addList
-        })
 
-    }
 
 
     return(
-<div className="wrapper">
+<div className="wrapper" >
 
 <h1>To Do List</h1>
 <input value={inpValue} onChange={(e)=>setInpValue(e.target.value)}/>
 {console.log(inpValue)}
 <button onClick={addListButton}>Add</button>
-<button onClick={ToDo}>Test</button>
+
 <ul>
     {addList.map(el=><li>{el.text}</li>)}
 
