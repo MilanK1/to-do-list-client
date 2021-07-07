b5264c1ec087544be91871e27df5532cfdf323e4
Milan <milankushnir@gmail.com>
2021-07-07 12:02:16 -0700

    visualized login page

--- src/Components/login/Login.js
+++ src/Components/login/Login.js
@@ -1,8 +1,20 @@
+import "./login.css"
 const Login =()=>{
 
     return(
-        <div className='container'>
-          <h1>This is login page!</h1>
+        <div className="container">
+            <div className="container__item">
+                <form className="form">
+                    <input type="input" className="form__field" placeholder="UserName" />
+                    <button type="button" className="btn btn--primary btn--inside uppercase"> </button>
+                    <input type="input" className="form__field" placeholder="Password" />
+                    <button type="button" className="btn btn--primary btn--inside uppercase"> </button>
+
+                </form>
+
+            </div>
+            <br/>
+            <button type="button" className="btn btn--primary btn--inside uppercase submit-btn">Submit</button>
         </div>
     )
 }
